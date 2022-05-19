d1a7629442d60395019832588b1f96ffb46da0dc
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-05-20 02:20:33 +0300

    closes mobile menu when clicking any of it's links, shows cart in the mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,16 +13,16 @@ function Header() {
                     <a href="/">Cake Shop</a>
                 </div>
                 <ul className={isMenuOpened ? "nav active" : "nav"}>
-                    <li className="nav__link">
+                    <li className="nav__link" onClick={() => {setMenuOpened(false)}}>
                         <HashLink smooth to="/#cakes">Торты</HashLink>
                     </li>
-                    <li className="nav__link">
+                    <li className="nav__link" onClick={() => {setMenuOpened(false)}}>
                         <HashLink smooth to="/#pastry">Пирожные</HashLink>
                     </li>
-                    <li className="nav__link">
+                    <li className="nav__link" onClick={() => {setMenuOpened(false)}}>
                         <HashLink smooth to="/#ice-cream">Мороженое</HashLink>
                     </li>
-                    <li className="nav__link">
+                    <li className="nav__link" onClick={() => {setMenuOpened(false)}}>
                         <a href="/">Напитки</a>
                     </li>
                     <li className="nav__link">
@@ -32,7 +32,7 @@ function Header() {
                         <Link to="/contacts">Контакты</Link>
                     </li>
                 </ul>
-                <div className="cart">
+                <div className={isMenuOpened ? "cart" : "cart hidden"}>
                     <Link to="/cart">Корзина</Link>
                 </div>
                 <button type="button" className={isMenuOpened ? "burger-menu-btn hidden" : "burger-menu-btn"} onClick={() => {setMenuOpened(true)}}>
