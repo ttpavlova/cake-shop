d0c9848986ae42c76acece9524b1ead634077fae
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-05-19 19:52:24 +0300

    responsive promo

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -7,9 +7,13 @@ function Promo() {
             <div className="promo__img-wrapper">
                 <div className="promo__img"></div>
             </div>
-            <div className="promo__text-wrapper">
-                <div className="promo__title">Delicious cakes</div>
-                <p className="promo__text">There are desserts for every taste.</p>
+            <div className="promo__left">
+                <div className="promo__container">
+                    <div className="promo__text-wrapper">
+                        <div className="promo__title">Delicious cakes</div>
+                        <p className="promo__text">There are desserts for every taste. There are desserts for every taste.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
