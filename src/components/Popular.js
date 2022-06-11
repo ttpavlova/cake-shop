9d7dd4c7c7d40c0aced59c66ba2fb3db3fdf8709
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-12 00:33:10 +0300

    added slick slider

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,8 +1,43 @@
 import React from "react";
 import "../css/Popular.css";
 import ProductCard from "./ProductCard";
+import Slider from "react-slick";
 
 function Popular(props) {
+
+    var settings = {
+        centerMode: true,
+        // centerPadding: '60px',
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        // autoplay: true,
+        autoplaySpeed: 10000,
+        speed: 500,
+        cssEase: 'linear',
+        draggable: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      };
+
     const popularItems = props.allData.filter(item => item.id === "cake2" || item.id === "cake3" || item.id === "pastry2" || item.id === "pastry3");
 
     const popularList = popularItems.map(item => (
@@ -20,10 +55,16 @@ function Popular(props) {
 
     return (
         <section className="popular">
-            <div className="popular__title">Популярные десерты</div>
-            <div className="cards">
-                {popularList}
+            <div className="popular__container">
+                <Slider {...settings}>
+                    {popularList}
+                </Slider>
+                <div className="popular__title">Популярные десерты</div>
             </div>
+            
+            {/* <div className="cards">
+                
+            </div> */}
         </section>
     );
 }
--- src/components/Promo.js
+++ src/components/Promo.js
@@ -10,8 +10,8 @@ function Promo() {
             <div className="promo__left">
                 <div className="promo__container">
                     <div className="promo__text-wrapper">
-                        <div className="promo__title">Delicious cakes</div>
-                        <p className="promo__text">There are desserts for every taste. There are desserts for every taste.</p>
+                        <div className="promo__title">Вкусные торты для всех</div>
+                        <p className="promo__text">Попробуйте наши десерты. Закажите онлайн с доставкой или заберите заказ в любом магазине сети.</p>
                     </div>
                 </div>
             </div>
