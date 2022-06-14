d98af1200ba3498cbfbbef2ef9e326977d49e352
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-14 08:27:46 +0300

    header styles in progress

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -11,14 +11,15 @@ function Promo() {
             <div className="promo__left">
                 <div className="promo__container">
                     <div className="promo__text-wrapper">
-                        <div className="promo__title">Вкусные торты для всех</div>
-                        <p className="promo__text">Попробуйте наши десерты. Закажите онлайн с доставкой или заберите заказ в любом магазине сети.</p>
+                        <div className="promo__title">Торты и десерты на любой вкус</div>
+                        <p className="promo__text">Попробуйте наши десерты. Закажите онлайн с доставкой или заберите заказ в любом магазине сети</p>
                         <button className="button promo__btn">
-                            <HashLink smooth to="/#popular" className="promo__link">Сделать заказ</HashLink>
-                            
-                            <span className="material-symbols-outlined arrow">
-                                trending_flat
-                            </span>
+                            <HashLink smooth to="/#popular" className="promo__link">
+                                Сделать заказ
+                                <span className="material-symbols-outlined arrow">
+                                    trending_flat
+                                </span>
+                            </HashLink>
                         </button>
                     </div>
                 </div>
