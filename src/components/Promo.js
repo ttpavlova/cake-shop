d279c7b3cfa2966eda908f55b48623d6900d0cf9
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-20 21:44:02 +0300

    fixed scrolling to dessert of the day section

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -15,7 +15,7 @@ function Promo() {
                         <div className="promo__title">Торты и десерты на любой вкус</div>
                         <p className="promo__text">Попробуйте наши десерты. Закажите онлайн с доставкой или заберите заказ в любом магазине сети</p>
                         <button className="button promo__btn">
-                            <HashLink smooth to="/#dessert-of-the-day" className="promo__link">
+                            <HashLink smooth to="/#promo-footer" className="promo__link">
                                 Сделать заказ
                                 <span className="material-symbols-outlined arrow">
                                     trending_flat
@@ -25,6 +25,7 @@ function Promo() {
                     </div>
                 </div>
             </div>
+            <div className="promo__footer" id="promo-footer"></div>
         </div>
     );
 }
