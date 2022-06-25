b85e6ff38b74f828eb38422ede427d326cb601ee
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-25 21:56:01 +0300

    styled 'about' page

--- src/components/About.js
+++ src/components/About.js
@@ -9,14 +9,34 @@ function About() {
             <div className="about__container">
                 <h2 className="about__title">О нас</h2>
                 <div className="about__content">
-                    <p>Наши усилия полностью направлены на создание десертов высокого качества. Мы используем только качественные и натуральные ингредиенты. (Мы используем только натуральные ингредиенты высокого качества). Готовим как известные кулинарные произведения: «Прага», «Наполеон», «Красный Бархат», так и современные интересные десерты.</p>
+                    <div className="about__text-part">
+                        <h3 className="about__subtitle">Наша цель</h3>
+                        <p className="about__text">Cake Shop — команда профессионалов, любящих своё дело и получающих удовольствие от создания красивых, вкусных и полезных тортов. Наши усилия полностью направлены на создание десертов высокого качества. Для изготовления десертов мы используем только натуральные ингредиенты высокого качества. Готовим как известные кулинарные произведения: «Прага», «Наполеон», «Красный Бархат», так и современные интересные десерты.</p>
+                        <p className="about__text">Наша миссия — дарить радость людям, собирая всех за одним столом.</p>
+                        <a className="about__link" href="/">
+                            <span className="about__link-text">Узнать о вакансиях</span>
+                            <span className="material-symbols-outlined arrow">
+                                trending_flat
+                            </span>
+                        </a>
+                    </div>
                     <div className="about__img-wrapper">
                         <img src={tartsImg1} className="about__img img1" alt="tarts"></img>
                     </div>
-                    <div className="about__img-wrapper">
+                    <div className="about__img-wrapper img2">
                         <img src={tartsImg2} className="about__img img2" alt="tarts"></img>
                     </div>
-                    <p>Наши усилия полностью направлены на создание десертов высокого качества. Мы используем только качественные и натуральные ингредиенты. (Мы используем только натуральные ингредиенты высокого качества).</p>
+                    <div className="about__text-part">
+                        <h3 className="about__subtitle">Разнообразие вариантов</h3>
+                        <p className="about__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="about__text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <a className="about__link" href="/">
+                            <span className="about__link-text">Найти нас на карте</span>
+                            <span className="material-symbols-outlined arrow">
+                                trending_flat
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
