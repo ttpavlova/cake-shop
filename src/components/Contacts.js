56756c794a0d0d0c72ff1463fc8749da9e13b93b
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-26 20:13:23 +0300

    address changed

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -22,14 +22,14 @@ function Contacts() {
 
                         <div className="contacts__item">
                             <p className="item__title">Адрес</p>
-                            <p className="item__property address">Санкт-Петербург, ул. Садовая, 29Д</p>
+                            <p className="item__property address">Санкт-Петербург, ул. Каменная, 29Д</p>
                             <p className="item__title">Режим работы</p>
                             <p className="item__property">Пн - Вс: 10:00 - 22:00</p>
                         </div>
 
                         <div className="contacts__item">
                             <p className="item__title">Адрес</p>
-                            <p className="item__property address">Санкт-Петербург, Суворовский пр., 2В</p>
+                            <p className="item__property address">Санкт-Петербург, Каштановый б-р, 2В</p>
                             <p className="item__title">Режим работы</p>
                             <p className="item__property">Пн - Вс: 09:00 - 21:00</p>
                         </div>
