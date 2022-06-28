4f68a8ddfba1235860006fa6e516c875d6b39f74
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-06-28 21:41:35 +0300

    changed some texts and the color of the header tags

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,8 @@ function About() {
                 <div className="about__content">
                     <div className="about__text-part">
                         <h3 className="about__subtitle">Наша цель</h3>
-                        <p className="about__text">Cake Shop — команда профессионалов, любящих своё дело и получающих удовольствие от создания красивых, вкусных и полезных тортов. Наши усилия полностью направлены на создание десертов высокого качества. Для изготовления десертов мы используем только натуральные ингредиенты высокого качества. Готовим как известные кулинарные произведения: «Прага», «Наполеон», «Красный Бархат», так и современные интересные десерты.</p>
-                        <p className="about__text">Наша миссия — дарить радость людям, собирая всех за одним столом.</p>
+                        <p className="about__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p className="about__text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <a className="about__link" href="#about">
                             <span className="about__link-text">Узнать о вакансиях</span>
                             <span className="material-symbols-outlined arrow">
