9d74e3b3aee1905c2d5d10ba89979ef5cc4e750c
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-07-01 01:55:29 +0300

    the anchor to 'dessert of the day' section fixed for mobile screens

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -4,6 +4,21 @@ import { HashLink } from 'react-router-hash-link';
 import promoImg from "../img/promo-bg-3.jpg";
 
 function Promo() {
+    function getAnchorPosition() {
+        let anchorId = "";
+
+        if (window.matchMedia("(min-width: 992px)").matches) {
+            // the viewport is at least 992 pixels wide
+            anchorId = "promo-footer";
+        }
+        else {
+            // the viewport is less than 992 pixels wide
+            anchorId = "dessert-of-the-day";
+          }
+
+        return anchorId;
+    }
+
     return (
         <div className="promo">
             <div className="promo__img-wrapper">
@@ -14,7 +29,7 @@ function Promo() {
                     <div className="promo__text-wrapper">
                         <h1 className="promo__title">Торты и десерты на любой вкус</h1>
                         <p className="promo__text">Наши десерты никого не оставят равнодушным. Закажите онлайн с доставкой или заберите заказ в любой кондитерской сети</p>
-                        <HashLink smooth to="/#promo-footer" className="btn promo__btn" role="button">
+                        <HashLink smooth to={"/#" + getAnchorPosition()} className="btn promo__btn" role="button">
                             Сделать заказ
                             <span className="material-symbols-outlined promo__arrow">
                                 trending_flat
